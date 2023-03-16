41033a98ec7884062fcf2309f62f15d4350e7447
Samuel <samuel16mendoza@gmail.com>
2023-03-16 11:27:50 -0600

    Login page added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Layout from "./pages/Layout";
 import Graphics from "./pages/Graphics";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div>
+      <Login />
+    </div>
+
+    /*<BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="graphics" element={<Graphics />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter>*/
   );
 }
 /* root.render(<App />);
