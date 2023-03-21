4d273a41128dbaca8479b726b5c243385be9c5e6
Samuel <samuel16mendoza@gmail.com>
2023-03-20 19:17:46 -0600

    Containers styles was modified
    Co-authored-by: Joana Miguel <Jmiguel@ucol@mx>
    Co-authored-by: Samuel Mendoza <Samuel16mendoza@gmail.com>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { TextField } from "@mui/material";
 import Box from "@mui/material/Box";
-import FormControl from '@mui/material/FormControl';
+import FormControl from "@mui/material/FormControl";
 /** inputLabel, input, inputadornment, iconButton,visibility, visibilyOff */
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import '../styles/LoginStyles.css';
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import "../styles/LoginStyles.css";
 
 const Login = (props) => {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -21,41 +21,82 @@ const Login = (props) => {
   };
   return (
     <div className="container">
-      <div className="container-info">
+      <div className="container-section1">
         <h1>Login</h1>
-        <img src={require('../images/Img-graphics1.png')} className="img-graphic1" alt="Grafica 1"/>
-        <img src={require('../images/Img-graphics2.png')} className="img-graphic2" alt="Grafica 2"/>
+        <img
+          src={require("../images/Img-graphics1.png")}
+          className="img-graphic1"
+          alt="Grafica 1"
+        />
+        <img
+          src={require("../images/Img-graphics2.png")}
+          className="img-graphic2"
+          alt="Grafica 2"
+        />
       </div>
-      <div className="container-inputs">
-      <img src={require('../images/Jesteria.png')} className="logo" alt="Logo"/>
-      <img src={require('../images/Flor.png')} className="img-flower" alt="Flower"/>
-      <img src={require('../images/Estrella.png')} className="img-star" alt="Star"/>
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'}}>
-          <TextField id="email-user" label="Email" sx={{m: 1, width: '25ch'}}/>
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
+      <div className="container-section2">
+        <div className="container-logo">
+          <img
+            src={require("../images/Jesteria.png")}
+            className="logo"
+            alt="Logo"
           />
-        </FormControl>
-        </Box>
+        </div>
+        <p className="txtWelcome">Bienvenido de nuevo</p>
+        <div className="container-img">
+          <div className="container-flower">
+            <img
+              src={require("../images/Flor.png")}
+              className="img-flower"
+              alt="Flower"
+            />
+          </div>
+          <div className="container-star">
+            <img
+              src={require("../images/Estrella.png")}
+              className="img-star"
+              alt="Star"
+            />
+          </div>
+        </div>
+
+        <div className="form-inputs">
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              id="email-user"
+              label="Email"
+              sx={{ m: 1, width: "60ch", marginBottom: "30px" }}
+            />
+            <FormControl sx={{ m: 1, width: "60ch" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </Box>
+        </div>
       </div>
     </div>
   );
