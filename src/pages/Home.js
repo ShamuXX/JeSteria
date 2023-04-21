c92133beab0a33816f05f92c71edb0b231fe84b2
Samuel <samuel16mendoza@gmail.com>
2023-04-20 19:30:40 -0600

    Structure from homePage
    Initial Structure to homePage for includ the necessary components.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,21 @@ import React from "react";
 import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import * as styles from "../styles/Home.module.css";
 
 const Home = () => {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>Joana mensA</h1>
-      <Button onClick={() => router.push("/Login")}>aaaa</Button>
+    <div className={styles.containerPage}>
+      <div className={styles.container1}>
+        <h1>Hola</h1>
+      </div>
+      <div className={styles.container2}>
+        <div className={styles.childrenContainer1}>
+          <h1>adiós</h1>
+        </div>
+      </div>
     </div>
   );
 };
