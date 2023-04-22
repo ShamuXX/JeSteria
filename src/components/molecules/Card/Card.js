edb5049198711e0a45a9c43c82e2a881b4861a60
LizyMiGo <jmiguel@ucol.mx>
2023-04-21 19:18:22 -0600

    modified expanded card styles

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -6,12 +6,12 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Card(){
-	const [state, setSate] = useState(false);
+	const [state, setSate] = useState(true);
   const onMorePress = () => setSate(!state);
   return(
     <div className={styles.container} style={{maxWidth: state ? "25%" : "100%"}}>
-      <p className={styles.title}>Actividades de hoy</p>
-        <div className={styles.containerPercentages}>
+      <p className={styles.title} style={{alignSelf: state ? "flex-start" : "center"}}>Actividades de hoy</p>
+        <div className={styles.containerPercentages} style={{width: state ? "100%" : "50%"}}>
           <div className={styles.containerCompleted}> 
             <p className={styles.title}>85%</p>
 						<p>Completado</p>
@@ -21,9 +21,11 @@ export default function Card(){
 						<p>Restante</p>
 					</div>
       	</div>
+			<div className={styles.containerTasks}>
 			<Box text={"Label 1"}/>
 			<Box text={"Label 2"}/>
 			<Box text={"Label 3"}/>
+			</div>
 			<p className={styles.more} onClick={onMorePress}>Ver más...</p>
     </div>
     )
