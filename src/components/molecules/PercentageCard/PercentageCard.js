0d901b20f3054a1d5a3f6d7d6982af693948b302
LizyMiGo <jmiguel@ucol.mx>
2023-04-26 01:19:14 -0600

    added circle graphic

--- src/components/molecules/PercentageCard/PercentageCard.js
+++ src/components/molecules/PercentageCard/PercentageCard.js
@@ -8,6 +8,10 @@ export default function PercentageCard(){
 			<div className={styles.containerBack1}></div>
       <div className={styles.containerBase}>
 				<p className={styles.title}>Media de actividades completadas por día</p>
+        <svg height="50%">
+          <circle className={styles.circle} r="75" cx="50%" cy="50%" pathLength="100"></circle>
+        </svg>
+        <p className={styles.percentage}>85%</p>
       </div>
    </div>      
 	)
