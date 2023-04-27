058aa91f24eca78a670ae3af1707583820a05964
Samuel <samuel16mendoza@gmail.com>
2023-04-27 16:53:42 -0600

    HeaderBar component was created
    HeaderBar component receives the basic properties to be able to interact with the application interface,
    such as browsing, managing texts and exchanging the user's image.

---
+++ src/components/atoms/HeaderBar/HeaderBar.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import * as styles from "./HeaderBar.module.css";
+import { NotificationsActive } from "@mui/icons-material";
+import { Button } from "@mui/material-next";
+
+export default function HeaderBar(props) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{props.title}</title>
+        <meta name={props.descripcion} content={props.content} />
+      </Head>
+      <header className={styles.header}>{props.header}</header>
+      <div className={styles.containerIcon}></div>
+      <div className={styles.containerImage}>
+        <NotificationsActive style={{ color: "#353535" }} />
+        <Button
+          style={{
+            height: "50%",
+            width: "0%",
+            marginLeft: "30%",
+          }}
+          href={props.href}
+        >
+          <Image
+            src={props.imgProfile}
+            alt="img-profile"
+            className={styles.profileImage}
+            style={{ width: "80px", height: "70px" }}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
