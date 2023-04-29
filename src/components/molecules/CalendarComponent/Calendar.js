5592cbb5f2ad6f39aa783b779c8d08f84a0d61e7
LizyMiGo <jmiguel@ucol.mx>
2023-04-28 18:08:33 -0600

    added date calendar from material ui

--- src/components/molecules/CalendarComponent/Calendar.js
+++ src/components/molecules/CalendarComponent/Calendar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import CalendarInfo from "../../atoms/CalendarInfo/CalendarInfo";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 export default function Calendar(){
 
 	return(
 		<div style={{width: "20%"}}>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<DateCalendar />
+			</LocalizationProvider>
 			<CalendarInfo text={"Días poco productivos"} color={"#EBCA75"}/>
 			<CalendarInfo text={"Días inproductivos"} color={"#E9486E"}/>
 			<CalendarInfo text={"Días productivos"} color={"#BEEB75"}/>  
