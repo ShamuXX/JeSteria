fc1c524c507bbdcd66f7215e737979213d7645fb
Samuel <samuel16mendoza@gmail.com>
2023-04-30 22:15:31 -0600

    improve code style
    Refactor component to handle better the styles.

--- src/components/atoms/ProgressionActivities/ProgressionActivities.js
+++ src/components/atoms/ProgressionActivities/ProgressionActivities.js
@@ -6,24 +6,12 @@ export default function ProgressionActivities(props) {
   const [iconActive, setIconActive] = useState(props.iconVisible);
   const [styleCard, setStyleCard] = useState(props.styleCard);
   return (
-    <>
-      {styleCard ? (
-        <div className={styles.container}>
-          {iconActive ? (
-            <CheckCircle className={styles.icon} fontSize="large" />
-          ) : null}
-          <p className={styles.text}>{props.text}</p>
-          <h3 className={styles.number}>{props.number}</h3>
-        </div>
-      ) : (
-        <div className={styles.container2}>
-          {iconActive ? (
-            <CheckCircle className={styles.icon} fontSize="large" />
-          ) : null}
-          <p className={styles.text}>{props.text}</p>
-          <h3 className={styles.number}>{props.number}</h3>
-        </div>
-      )}
-    </>
+    <div className={styleCard ? styles.container : styles.container2}>
+      {iconActive ? (
+        <CheckCircle className={styles.icon} fontSize="large" />
+      ) : null}
+      <p className={styles.text}>{props.text}</p>
+      <h3 className={styles.number}>{props.number}</h3>
+    </div>
   );
 }
