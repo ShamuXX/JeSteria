c243007c37f00ada714fa524fd99ee48651d753a
ShamuXX <samuel16mendoza@gmail.com>
2023-06-12 08:24:15 -0600

    Update useAuthUser.js

--- src/hooks/useAuthUser.js
+++ src/hooks/useAuthUser.js
@@ -2,22 +2,17 @@ import { useEffect, useContext } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/router";
 import { auth } from "../firebase/firebaseApp";
-import AuthContext from "../context/AuthContext";
 
 export const useAuthUser = () => {
   const { push, pathname } = useRouter();
 
-  const { setisLogged } = useContext(AuthContext);
-
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      let userLogged = user === null ? false : true;
-
+      const userLogged = user !== null;
+      
       if (!userLogged) {
         push("/login");
-        setisLogged(false);
       } else {
-        setisLogged(true);
         if (pathname === "/login") {
           push("/");
         }
