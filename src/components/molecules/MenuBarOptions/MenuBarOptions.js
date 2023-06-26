4932a976a9733268f227b59bd77a7f6e8ea7655e
LizyMiGo <jmiguel@ucol.mx>
2023-06-26 10:59:11 -0600

    added logout function and button redesign

--- src/components/molecules/MenuBarOptions/MenuBarOptions.js
+++ src/components/molecules/MenuBarOptions/MenuBarOptions.js
@@ -1,7 +1,19 @@
 import MenuButtonIcon from "../../atoms/MenuButtonIcon/MenuButtonIcon";
 import * as styles from "./MenuBarOptions.module.css";
+import Button from "@mui/material/Button";
+import { auth } from "../../../firebase/firebaseApp";
+import {signOut } from "firebase/auth";
+import { useRouter } from "next/router";
+import Logout from "@mui/icons-material/Logout";
 
 export default function MenuBarOption() {
+  const { push } = useRouter();
+  const logout = () => {
+    signOut(auth)
+  .then(() => {
+    push("/Login")
+  })
+  }
   return (
     <div className={styles.container}>
       <MenuButtonIcon text="Home" icon="Home" navigate="/Login" />
@@ -27,8 +39,14 @@ export default function MenuBarOption() {
         navigate="/Login"
       />
       <MenuButtonIcon text="Achievements" icon="NorthEast" navigate="/Login" />
-      <div className={styles.containerLogOutButton}>
-        <MenuButtonIcon text="Log Out" icon="Logout" navigate="/Login" />
+      <div className={styles.containerLogOutButton} onClick={logout}>
+        <div className={styles.containerLogOutInfo}>
+        <Logout />
+        <Button 
+          sx={{color:"#626262", marginBottom: "1em"}}> 
+          Log Out 
+        </Button>
+        </div>
       </div>
     </div>
   );
