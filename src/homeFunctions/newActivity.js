165dc632b76097898bc4648a56657da2f8315202
Samuel <samuel16mendoza@gmail.com>
2023-07-18 21:06:27 -0600

    Save values to Activity object
    Values of name, description and icon was saved to the state.
    
    Co-authored-by: Joana Miguel <Jmiguel@ucol.mx>

--- src/homeFunctions/newActivity.js
+++ src/homeFunctions/newActivity.js
@@ -1,6 +1,7 @@
 import { TextField } from "@mui/material";
 import React, { useState } from "react";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
+import { Input } from "@mui/material-next";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
@@ -8,35 +9,74 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { Home, TrendingUp, AssignmentInd } from "@mui/icons-material";
-import * as styles from "../styles/homeFunctions/newActivity.module.css";
+import * as styles from "./newActivity.module.css";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(localizedFormat);
+dayjs.extend(customParseFormat);
 
 export default function NewActity() {
-  const [icon, setIcon] = useState("");
-  const handleChange = (event, SelectChangeEvent) => {
-    setIcon(event.target.value);
+  const [activity, setActivity] = useState({
+    name: "",
+    description: "",
+    date: null,
+    icon: "",
+  });
+  console.log(activity);
+  const changeActivity = (e) => {
+    setActivity({
+      ...activity,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleDateChange = (date) => {
+    let selectedDate = dayjs(date).format("YYYY-MM-DD");
+    setActivity({ ...activity, date: date });
   };
   return (
     <div className={styles.container}>
-      <div>
-        <p>Name of Activity</p>
-        <TextField id="Name" name="Name" label="Name" />
-        <p>Description</p>
-        <TextareaAutosize
-          aria-label="minimum height"
-          minRows={5}
-          placeholder="Minimum 3 rows"
+      <div className={styles.containerFirst}>
+        <p className={styles.title}>Name of Activity</p>
+        <TextField
+          id="name"
+          name="name"
+          label="name"
+          size="small"
+          className={styles.inputName}
+          onChange={changeActivity}
+        />
+        <p className={styles.title}>Description</p>
+        <textarea
+          id="description"
+          name="description"
+          onChange={changeActivity}
+          style={{
+            width: "96%",
+            height: "200px",
+            padding: "10px",
+            fontSize: "16px",
+            border: "1px solid #ccc",
+            borderRadius: "4px",
+          }}
         />
       </div>
-      <div>
-        <p>Designate date</p>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar />
-        </LocalizationProvider>
-        <p>Icon</p>
+
+      <div className={styles.containerSecond}>
+        <p className={styles.title}>Designate date</p>
+        <div className={styles.containerCalendar}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar value={activity.date} onChange={handleDateChange} />
+          </LocalizationProvider>
+        </div>
+        <p className={styles.title}>Icon</p>
         <FormControl sx={{ m: 1, minWidth: 120 }}>
           <Select
-            value={icon}
-            onChange={handleChange}
+            id="icon"
+            name="icon"
+            value={activity.icon}
+            onChange={changeActivity}
             displayEmpty
             inputProps={{ "aria-label": "Without label" }}
           >
