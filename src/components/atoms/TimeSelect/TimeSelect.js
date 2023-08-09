8f52546ac5dca7f3f1a071ad846ae4a3557ddeca
LizyMiGo <jmiguel@ucol.mx>
2023-08-09 14:46:56 -0600

    Add active selectors for hour and minute buttons
    Co-authored-by: Samuel Mendoza <Samuel16mendoza@gmail.com>

--- src/components/atoms/TimeSelect/TimeSelect.js
+++ src/components/atoms/TimeSelect/TimeSelect.js
@@ -1,46 +1,55 @@
-import * as styles from "./TimeSelect.module.css"
-import button from "react"
+import * as styles from "./TimeSelect.module.css";
+import { useState } from "react";
 
-export default function TimeSelect({text}) {
-    const timeValHour = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12, 13, 14];
-    const timeValMinutes = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+export default function TimeSelect({ text }) {
+  const timeValHour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const timeValMinutes = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
 
-    const onHandleClick = () => {
-      
-    }
+  const [stateHour, setStateHour] = useState(true);
+  const [stateMinute, setStateMinute] = useState(true);
 
-    const hour = timeValHour.map((hour, Index) => {
-      return(
-        <button key={Index} className={styles.buttonTime}>
-          {hour}
-        </button>    
-      )
-    })
-    const minutes = timeValMinutes.map((minutes, Index) => {
-      return(
-        <button key={Index} className={styles.buttonTime} onClick={onHandleClick}>
-          {minutes}
-        </button>   
-      )
-    })
-    return(
-      <div className={styles.containerAllElements}>
-        <div className={styles.main}>
-          <p className={styles.text}>{text}</p>
-          <div className={styles.container}>
-            <div className={styles.hourContainer}>
-                {hour}
-            </div>
-            <div className={styles.minutesContainer}>
-              {minutes}
-            </div>
+  const handleHour = (index) => () => setStateHour(index);
+  const handleMinute = (index) => () => setStateMinute(index);
+  console.log("Hora", stateHour);
+  console.log("Minuto", stateMinute);
+
+  const hour = timeValHour.map((hour, Index) => {
+    const isActive = Index === stateHour ? styles.active : "";
+    const className = [styles.buttonHour, isActive].join(" ");
+
+    return (
+      <button key={Index} className={className} onClick={handleHour(Index)}>
+        {hour}
+      </button>
+    );
+  });
+  const minutes = timeValMinutes.map((minutes, Index) => {
+    const isActive = Index === stateMinute ? styles.active : "";
+    const className = [styles.buttonMinute, isActive].join(" ");
+
+    return (
+      <button key={Index} className={className} onClick={handleMinute(Index)}>
+        {minutes}
+      </button>
+    );
+  });
+  return (
+    <div className={styles.containerAllElements}>
+      <header className={styles.main}>
+        <p className={styles.text}>{text}</p>
+        <div className={styles.container}>
+          <div className={styles.hourContainer}>{hour}</div>
+          <div className={styles.minutesContainer}>{minutes}</div>
         </div>
+      </header>
+      <div className={styles.containerFormats}>
+        <div className={styles.formatTime}>
+          <p>Am</p>
         </div>
-        <div className={styles.containerFormats}>
-          <div className={styles.formatTime}><p>Am</p></div>
-          <div className={styles.formatTime}><p>Pm</p></div>
+        <div className={styles.formatTime}>
+          <p>Pm</p>
         </div>
       </div>
-        
-    )
+    </div>
+  );
 }
