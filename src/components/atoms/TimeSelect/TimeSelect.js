782aec3ae3f59a01e570ddc3c80567ead8425cb0
LizyMiGo <jmiguel@ucol.mx>
2023-08-10 13:43:02 -0600

    Asign values and fix some styles
    Asign values to hour and minutes component, and fix proportion of AM and
    PM section
    
    Co-authored-by: Samuel Mendoza <Samuel16mendoza@gmail.com>

--- src/components/atoms/TimeSelect/TimeSelect.js
+++ src/components/atoms/TimeSelect/TimeSelect.js
@@ -2,16 +2,20 @@ import * as styles from "./TimeSelect.module.css";
 import { useState } from "react";
 
 export default function TimeSelect({ text }) {
-  const timeValHour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  const timeValMinutes = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
+  const timeValHour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const timeValMinutes = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 
   const [stateHour, setStateHour] = useState(true);
   const [stateMinute, setStateMinute] = useState(true);
+  const [indicator, setIndicator] = useState("");
 
   const handleHour = (index) => () => setStateHour(index);
   const handleMinute = (index) => () => setStateMinute(index);
-  console.log("Hora", stateHour);
-  console.log("Minuto", stateMinute);
+  function handleIndicator(val) {
+    setIndicator(val);
+  }
+  const Hour = timeValHour[stateHour];
+  const Minutes = timeValMinutes[stateMinute];
 
   const hour = timeValHour.map((hour, Index) => {
     const isActive = Index === stateHour ? styles.active : "";
@@ -43,11 +47,19 @@ export default function TimeSelect({ text }) {
         </main>
       </header>
       <section>
-        <div>
-          <p>Am</p>
+        <div
+          onClick={() => {
+            handleIndicator("AM");
+          }}
+        >
+          <p>AM</p>
         </div>
-        <div>
-          <p>Pm</p>
+        <div
+          onClick={() => {
+            handleIndicator("PM");
+          }}
+        >
+          <p>PM</p>
         </div>
       </section>
     </div>
